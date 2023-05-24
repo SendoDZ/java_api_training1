6922ba5869029f80239b47a0d161dc0472a3209c
Icham BENABBAS <icham.benabbas@epita.fr>
2023-05-24 19:15:39 +0200

    Partie 2, Version 1. Un seul test à été éffectué (Concluant)

--- fr/lernejo/navy_battle/Launcher.java
+++ fr/lernejo/navy_battle/Launcher.java
@@ -1,6 +1,40 @@
 package fr.lernejo.navy_battle;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Launcher {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        int port = Integer.parseInt(args[0]);
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.setExecutor(createExecutorService());
+
+        server.createContext("/ping", new PingHandler());
+
+        server.start();
+        System.out.println("Server started on port " + port);
+    }
+
+    private static ExecutorService createExecutorService() {
+        return Executors.newFixedThreadPool(1);
+    }
+
+    static class PingHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "OK";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        }
     }
 }
