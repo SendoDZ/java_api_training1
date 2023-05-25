158e75420946fd7ca7818da479396f42360b1234
Icham BENABBAS <icham.benabbas@epita.fr>
2023-05-25 17:51:08 +0200

    Reglage d'une erreur

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -4,29 +4,44 @@ import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Launcher {
-
     public static void main(String[] args) throws IOException {
-        if (args.length != 1) {
-            System.out.println("Usage: Launcher [port]");
-            System.exit(-1);
-        }
         int port = Integer.parseInt(args[0]);
-        startServer(port);
+        HttpServer server = createServer(port);
+        configureContexts(server, port);
+        startServer(server);
+        startClientIfRequested(args, port);
     }
 
-    private static void startServer(int port) throws IOException {
+    private static HttpServer createServer(int port) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-        server.setExecutor(Executors.newFixedThreadPool(1));
-        createContexts(server, port);
+        ExecutorService executor = Executors.newFixedThreadPool(1);
+        server.setExecutor(executor);
+        return server;
+    }
+
+    private static void configureContexts(HttpServer server, int port) {
+        server.createContext("/ping", new PingHandler());
+        server.createContext("/api/game/start", new GameStartHandler(port));
+    }
+
+    private static void startServer(HttpServer server) {
         server.start();
     }
 
-    private static void createContexts(HttpServer server, int port) {
-        server.createContext("/ping", new PingHandler());
-        server.createContext("/api/game/start", new GameStartHandler(port));
+    private static void startClientIfRequested(String[] args, int port) {
+        if (args.length > 1) {
+            String adversaryUrl = args[1];
+            BattleGameClient battleGameClient = new BattleGameClient();
+            try {
+                battleGameClient.sendStartGameRequest(adversaryUrl, port);
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
 
